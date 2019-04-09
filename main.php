ddc41e393f545cc31d208e145f0ea02f95ec67b9
nikhil.t <nikhil.t@notifyvisitors.com>
2019-04-09 17:39:05 +0530

    test

---
+++ main.php
@@ -0,0 +1,18 @@
+<style>
+    body {
+        backgound-color: grey;
+        color: blue;
+    }
+</style>
+
+<pre>
+<?php
+    $a = $_SERVER['REQUEST_SCHEME'] . 's://' . $_SERVER['SERVER_NAME'];
+?>
+<!-- <a href="<?=$a?>"> home </a>  <a href="<?=$a?>/trustedweb/product/one/"> one index </a>  <a href="<?=$a?>/trustedweb/product/one/one.php"> one.php </a>  <a href="<?=$a?>/trustedweb/product/two/"> two index </a> <a href="<?=$a?>/trustedweb/product/two/two.php"> two.php </a>   -->
+
+<a href="<?=$a?>"> home </a>  <a href="<?=$a?>/product/one/"> one index </a>  <a href="<?=$a?>/product/one/one.php"> one.php </a>  <a href="<?=$a?>/product/two/"> two index </a> <a href="<?=$a?>/product/two/two.php"> two.php </a>  
+
+
+
+<h1><?=$_SERVER['REQUEST_URI']?></h1>
