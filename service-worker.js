b371d7b173ab5d0e7fe085f4b36f4017ad009160
nikhilnotify <49435223+nikhilnotify@users.noreply.github.com>
2019-05-31 13:27:21 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+var version = '0.3';
+var amp_version = '0.3';
+var NOTIFYVISITORS_BRAIND_ID = '1431';
+var _nvClientConfig = {
+    auth: {
+        bid: '1431',
+        bid_e: '8CC4992A2A18BE7BD71E6A499F8CDB50',
+        t: "420"
+    },
+    data: {},
+    cPubKey: '',
+}
+
+importScripts('https://cdnp.notifyvisitors.com/js/brand_hosted/amp-push-worker.js');
+importScripts('https://cdnp.notifyvisitors.com/js/brand_hosted/push-worker.js');
+importScripts('https://s3.amazonaws.com/notifypush/cache_worker/config-1431.js');
+importScripts('https://cdnp.notifyvisitors.com/js/brand_hosted/cache-worker.js');
