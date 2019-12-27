f2def2ffc0dc3a03802096ea1ecc1dee82be8f97
nikhilnotify <49435223+nikhilnotify@users.noreply.github.com>
2019-12-27 16:09:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 
-    include_once 'main.php';
+if($_POST){
+	echo '{"A":"A"}';
+}
+return;
+
+
+include 'main.php';
